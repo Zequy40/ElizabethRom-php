31ec63798f9d3ed4b2ca46d3023889eab0809f37
ZekiDev <47107701+Zequy40@users.noreply.github.com>
2024-02-24 11:42:15 +0100

    number of invoice added

--- frontend/checkin.php
+++ frontend/checkin.php
@@ -10,9 +10,12 @@ if (isset($_SESSION['user_id'])) {
 
 include '_backAdmin/conexion/conexion.php';
 $pid = $user_id;
+$num = mt_rand(); 
+$invoice = 'p-2024/'.$num;
+
      //Actualiza el estado del pedido
-            $update_product = $conn->prepare("UPDATE orders SET orderStatus = ?, send = ? WHERE userId = ? AND orderStatus = 0");
-            $update_product->execute([1, 0, $pid]);
+            $update_product = $conn->prepare("UPDATE orders SET orderStatus = ?, send = ?, invoiced = ? WHERE userId = ? AND orderStatus = 0");
+            $update_product->execute([1, 0,$invoice, $pid]);
             header("location:success.html");
 			exit;
 
